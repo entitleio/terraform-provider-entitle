c0a028c784a45080c4c6f49d533104311495620f
nissim-infra <bitan.nissim@gmail.com>
2024-09-18 15:14:16 +0300

    add message when unauthorized access and add validator

--- internal/validators/uuid.go
+++ internal/validators/uuid.go
@@ -32,26 +32,3 @@ func (u UUID) ValidateString(ctx context.Context, req validator.StringRequest, r
 		)
 	}
 }
-
-type WorkflowName struct{}
-
-// Description satisfies the validator.String interface.
-func (u WorkflowName) Description(ctx context.Context) string {
-	return "validating the resource id is workflow name length between 2-50"
-}
-
-// MarkdownDescription satisfies the validator.String interface.
-func (u WorkflowName) MarkdownDescription(ctx context.Context) string {
-	return "validating the resource id is workflow name length between 2-50"
-}
-
-// ValidateString Validate satisfies the validator.String interface.
-func (u WorkflowName) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
-	val := req.ConfigValue.ValueString()
-	if len(val) < 2 || len(val) > 50 {
-		resp.Diagnostics.AddError(
-			"Workflow Name Validate Failed",
-			fmt.Sprintf("validating the resource id is workflow name length between 2-50, name: (%s)", val),
-		)
-	}
-}
