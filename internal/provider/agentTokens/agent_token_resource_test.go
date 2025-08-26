d23d4de696eb4ec46d10c717f741afbcb457111e
arturwwl <32646237+arturwwl@users.noreply.github.com>
2025-08-26 10:10:02 +0200

    docs(ER-2759): data sources examples (#41)

---
+++ internal/provider/agentTokens/agent_token_resource_test.go
@@ -0,0 +1,50 @@
+//go:build acceptance
+// +build acceptance
+
+package agentTokens_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/entitleio/terraform-provider-entitle/internal/testhelpers"
+)
+
+func TestAgentTokenResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testhelpers.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testhelpers.ProviderConfig + `
+
+resource "entitle_agent_token" "my_agent_token" {
+	name = "My Agent Token"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify
+					resource.TestCheckResourceAttr("entitle_agent_token.my_agent_token", "name", "My Agent Token"),
+
+					// Verify dynamic values have any value set in the state.
+					resource.TestCheckResourceAttrSet("entitle_agent_token.my_agent_token", "id"),
+					resource.TestCheckResourceAttrSet("entitle_agent_token.my_agent_token", "token"),
+				),
+			},
+			// Update testing
+			{
+				Config: testhelpers.ProviderConfig + `
+
+resource "entitle_agent_token" "my_agent_token" {
+	name = "My Agent Token UPDATED"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify
+					resource.TestCheckResourceAttr("entitle_agent_token.my_agent_token", "name", "My Agent Token UPDATED"),
+				),
+			},
+		},
+	})
+}
