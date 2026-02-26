7c58733fbbbd082356050ea83acc7dedc037d3b8
Carter Brainerd <carter.brainerd@sourcegraph.com>
2026-02-26 11:30:39 -0500

    fix: use ValueString instead of String for workflow notified entities

--- internal/provider/workflows/utils.go
+++ internal/provider/workflows/utils.go
@@ -236,7 +236,7 @@ func getWorkflowsRules(
 						err := t.FromApprovalEntityUserSchema(client.ApprovalEntityUserSchema{
 							Type: client.EnumApprovalEntityUserUserUser,
 							Entity: client.UserEntitySchema{
-								Id: target.Id.String(),
+								Id: target.Id.ValueString(),
 							},
 						})
 						if err != nil {
@@ -271,7 +271,7 @@ func getWorkflowsRules(
 						err := t.FromApprovalEntityGroupSchema(client.ApprovalEntityGroupSchema{
 							Type: client.DirectoryGroup,
 							Entity: client.GroupEntitySchema{
-								Id: target.ID.String(),
+								Id: target.ID.ValueString(),
 							},
 						})
 						if err != nil {
@@ -306,7 +306,7 @@ func getWorkflowsRules(
 						err := t.FromApprovalEntityScheduleSchema(client.ApprovalEntityScheduleSchema{
 							Type: client.OnCallIntegrationSchedule,
 							Entity: client.ScheduleEntitySchema{
-								Id: target.ID.String(),
+								Id: target.ID.ValueString(),
 							},
 						})
 						if err != nil {
